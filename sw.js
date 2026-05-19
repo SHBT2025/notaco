3c5cae17bba73465900fec67c48c050a4418373b
SHBT2025 <zgzongbiaotou@gmail.com>
2026-05-19 14:15:12 +0800

    Add files
    Add files 6

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'notaco-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './notaco2.html',
+  './12.png',
+  './192.png',
+  './512.png',
+  './manifest.json'
+];
+
+// 安装事件 - 缓存资源
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 激活事件 - 清理旧缓存
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// 请求拦截 - 优先返回缓存
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  if (!event.request.url.startsWith(self.location.origin)) return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          })
+          .catch(() => {
+            if (event.request.destination === 'document') {
+              return caches.match('./notaco2.html');
+            }
+          });
+      })
+  );
+});
